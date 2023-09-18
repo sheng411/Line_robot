b1952c05c97ddf1a8e1be447ce92499b2a4ead7d
sheng411 <isu11003058A@go.isu.edu.tw>
2023-09-19 02:14:35 +0800

    測試用

---
+++ bot1.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from a_token import iot_token,iot_secret
+# 載入 json 標準函式庫，處理回傳的資料格式
+import json
+
+# load LINE Message API Related Libraries
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    try:
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = iot_token
+        secret = iot_secret
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
+        if type=='text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)                                       # 印出內容
+            reply = "你說的是:"+msg
+        else:
+            reply = 'Unrecognizable!!'
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
+    except:
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+
+if __name__ == "__main__":
+    app.run()
