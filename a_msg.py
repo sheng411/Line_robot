2bf249905b8dcbd0037110e62c17c9d56a41aa6f
sheng411 <isu11003058A@go.isu.edu.tw>
2023-09-19 00:09:54 +0800

    訊息

---
+++ a_msg.py
@@ -0,0 +1,12 @@
+msg={
+    'hi':'Hi! 你好呀~',
+    'hello':'Hello!!!!',
+    '你好':'你好～',
+    '安安':'安安說安安',
+    '氣象專家':'嘿 叫我嗎?',
+    '馬卡巴卡':'https://www.youtube.com/watch?v=hGDAU_AZZok',
+    'help':'你可以傳給我 文字/地點/原始貼圖/圖片/聲音/影片/資訊面板/地震資訊/特殊指令',
+    '回家山線':'!! 注意 !! 中油生產路站有"自助加油" \n總里程:45.9K \nhttps://goo.gl/maps/WskMQwJXf6uP6rZf9',
+    '回家海線':'!! 注意 !! 路途沒有"自助加油" \n總里程:47.5K \nhttps://goo.gl/maps/E8Zyvf42wsh756HH9',
+    '回學校':'!! 注意 !! 中油橋頭站有"自助加油" \n總里程:45.8K \nhttps://goo.gl/maps/seGtHHMQ3wCHkFrr6',
+}
