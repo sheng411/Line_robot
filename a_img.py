caa426b8396ba600b68533bf05dcfaa44fec463a
sheng411 <isu11003058A@go.isu.edu.tw>
2023-09-19 00:09:44 +0800

    圖片

---
+++ a_img.py
@@ -0,0 +1,4 @@
+img={
+    '皮卡丘':'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png',
+    '傑尼龜':'https://upload.wikimedia.org/wikipedia/en/5/59/Pok%C3%A9mon_Squirtle_art.png'
+}
