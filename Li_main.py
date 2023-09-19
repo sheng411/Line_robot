184c85c498baa8d06b1929a0475cdb4a13e20505
sheng411 <isu11003058A@go.isu.edu.tw>
2023-09-19 11:23:38 +0800

    新增天氣函式，並把push msg中reply msg獨立成function

--- Li_main.py
+++ Li_main.py
@@ -5,9 +5,8 @@ from a_msg import msg
 from flask import Flask, request
 import json
 from linebot import LineBotApi, WebhookHandler
-# 載入對應的函式庫
 from linebot.models import TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage,PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
-import requests,time
+import requests,time,statistics
 
 '''
 若有新增資訊都需要重新執行
@@ -37,9 +36,16 @@ def linebot():
         else:
             userid = json_data['events'][0]['source']['userId']  # 取得 userid
 
+        if 'message' in json_data['events'][0]:
+            if json_data['events'][0]['message']['type'] == 'location':
+                address = json_data['events'][0]['message']['address'].replace('台','臺')
+                # 回覆爬取到的相關氣象資訊
+                reply_msg(f'{address}\n\n{current_weather(address)}', reply_token, access_token)
+                print(address)
+
         if tp == 'text':
             # 如果是文字類型的訊息
-            msg = reply_msg(json_data['events'][0]['message']['text'])   # 取出文字並對應到 reply_msg 的函式
+            msg = check_msg(json_data['events'][0]['message']['text'])   # 取出文字並對應到 reply_msg 的函式
             if msg[0] == 'text':
                 msg_ck(msg[1])
                 '''
@@ -92,8 +98,9 @@ def linebot():
     except:
         print('error', body)
     return 'OK'
+
 # 定義回覆訊息的函式
-def reply_msg(text):
+def check_msg(text):
     global text_ck
     # 預設回覆的文字就是收到的訊息
     reply_msg_content = ['text',text]
@@ -266,12 +273,89 @@ def earth(url):
         data = requests.post(l_notify_url, headers=headers, data=data)    # 發送 LINE NOtify
         '''
         #push data
-        push_msg(msg,img)
+        push_msg(img)
+        reply_msg(msg)
 
         break
 
+# 目前天氣函式
+def current_weather(address):
+    city_list, area_list, area_list2 = {}, {}, {} # 定義好待會要用的變數
+    msg = '找不到氣象資訊。'                         # 預設回傳訊息
+
+    # 定義取得資料的函式
+    def get_data(url):
+        w_data = requests.get(url)   # 爬取目前天氣網址的資料
+        w_data_json = w_data.json()  # json 格式化訊息內容
+        location = w_data_json['cwbopendata']['location']  # 取出對應地點的內容
+        for i in location:
+            name = i['locationName']                       # 測站地點
+            city = i['parameter'][0]['parameterValue']     # 縣市名稱
+            area = i['parameter'][2]['parameterValue']     # 鄉鎮行政區
+            temp = check_data(i['weatherElement'][3]['elementValue']['value'])                       # 氣溫
+            humd = check_data(round(float(i['weatherElement'][4]['elementValue']['value'] )*100 ,1)) # 相對濕度
+            r24 = check_data(i['weatherElement'][6]['elementValue']['value'])                        # 累積雨量
+            if area not in area_list:
+                area_list[area] = {'temp':temp, 'humd':humd, 'r24':r24}  # 以鄉鎮區域為 key，儲存需要的資訊
+            if city not in city_list:
+                city_list[city] = {'temp':[], 'humd':[], 'r24':[]}       # 以主要縣市名稱為 key，準備紀錄裡面所有鄉鎮的數值
+            city_list[city]['temp'].append(temp)   # 記錄主要縣市裡鄉鎮區域的溫度 ( 串列格式 )
+            city_list[city]['humd'].append(humd)   # 記錄主要縣市裡鄉鎮區域的濕度 ( 串列格式 )
+            city_list[city]['r24'].append(r24)     # 記錄主要縣市裡鄉鎮區域的雨量 ( 串列格式 )
+
+    # 定義如果數值小於 0，回傳 False 的函式
+    def check_data(e):
+        return False if float(e)<0 else float(e)
+
+    # 定義產生回傳訊息的函式
+    def msg_content(loc, msg):
+        a = msg
+        for i in loc:
+            if i in address: # 如果地址裡存在 key 的名稱
+                temp = f"氣溫 {loc[i]['temp']} 度，" if loc[i]['temp'] != False else ''
+                humd = f"相對濕度 {loc[i]['humd']}%，" if loc[i]['humd'] != False else ''
+                r24 = f"累積雨量 {loc[i]['r24']}mm" if loc[i]['r24'] != False else ''
+                description = f'{temp}{humd}{r24}'.strip('，')
+                a = f'{description}。' # 取出 key 的內容作為回傳訊息使用
+                break
+        return a
+
+    try:
+        get_data(weather_1_url)
+        get_data(weather_2_url)
+
+        for i in city_list:
+            if i not in area_list2: # 將主要縣市裡的數值平均後，以主要縣市名稱為 key，再度儲存一次，如果找不到鄉鎮區域，就使用平均數值
+                area_list2[i] = {'temp':round(statistics.mean(city_list[i]['temp']),1),
+                                'humd':round(statistics.mean(city_list[i]['humd']),1),
+                                'r24':round(statistics.mean(city_list[i]['r24']),1)
+                                }
+        msg = msg_content(area_list2, msg)  # 將訊息改為「大縣市」
+        msg = msg_content(area_list, msg)   # 將訊息改為「鄉鎮區域」
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
+
 #push message function
-def push_msg(msg,img):
+def push_msg(img):
+    headers = {
+        'Authorization':'Bearer '+iot_token,
+        'Content-Type':'application/json'
+    }
+
+    #push img
+    ibody = {
+        'to':userid,
+        'messages':[{
+            'type': 'image',
+            'originalContentUrl':img,
+            'previewImageUrl':img
+            }]
+        }
+    ireq = requests.request('POST', l_push_url,headers=headers,data=json.dumps(ibody).encode('utf-8'))
+
+#reply message function
+def reply_msg(msg):
     headers = {
         'Authorization':'Bearer '+iot_token,
         'Content-Type':'application/json'
@@ -285,18 +369,8 @@ def push_msg(msg,img):
                 'text':msg
             }]
     }
-    
-    #push img
-    ibody = {
-        'to':userid,
-        'messages':[{
-            'type': 'image',
-            'originalContentUrl':img,
-            'previewImageUrl':img
-            }]
-        }
-    req = requests.request('POST', l_reply_url,headers=headers,data=json.dumps(body).encode('utf-8'))
-    ireq = requests.request('POST', l_push_url,headers=headers,data=json.dumps(ibody).encode('utf-8'))
+    req= requests.request('POST', l_reply_url,headers=headers,data=json.dumps(body).encode('utf-8'))
+
 
 if __name__ == "__main__":
     app.run()
