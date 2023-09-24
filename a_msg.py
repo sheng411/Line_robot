5d5a11e4d4913a566fe5d7e10a965c1567102300
sheng411 <isu11003058A@go.isu.edu.tw>
2023-09-24 13:20:59 +0800

    新增中油油價查詢關鍵字，並修改順序

--- a_msg.py
+++ a_msg.py
@@ -6,8 +6,9 @@ msg={
     '氣象專家':'嘿 叫我嗎?',
     '馬卡巴卡':'https://www.youtube.com/watch?v=hGDAU_AZZok',
     '晚安':'晚安，馬卡巴卡\n\nhttps://www.youtube.com/watch?v=hGDAU_AZZok',
-    'help':'你可以傳給我 文字/地點/原始貼圖/圖片/聲音/影片/資訊面板/地震資訊/特殊指令',
+    '油價':'中油油價連結:\nhttps://www.cpc.com.tw/historyprice.aspx?n=2890',
     '回家山線':'!! 注意 !! 中油生產路站有"自助加油" \n總里程:45.9K \nhttps://goo.gl/maps/WskMQwJXf6uP6rZf9',
     '回家海線':'!! 注意 !! 路途沒有"自助加油" \n總里程:47.5K \nhttps://goo.gl/maps/E8Zyvf42wsh756HH9',
     '回學校':'!! 注意 !! 中油橋頭站有"自助加油" \n總里程:45.8K \nhttps://goo.gl/maps/seGtHHMQ3wCHkFrr6',
+    'help':'你可以傳給我 文字/地點/原始貼圖/圖片/聲音/影片/資訊面板/地震資訊/特殊指令',
 }
