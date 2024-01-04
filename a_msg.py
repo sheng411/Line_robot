6c5e9f7e9ab2d58bb5527548a051ca3b9e1ec63f
sheng411 <isu11003058A@go.isu.edu.tw>
2024-01-05 03:15:48 +0800

    update keyword("油價" to "中油油價")

--- a_msg.py
+++ a_msg.py
@@ -6,7 +6,7 @@ msg={
     '氣象專家':'嘿 叫我嗎?',
     '馬卡巴卡':'https://www.youtube.com/watch?v=hGDAU_AZZok',
     '晚安':'晚安，馬卡巴卡\n\nhttps://www.youtube.com/watch?v=hGDAU_AZZok',
-    '油價':'中油油價連結:\nhttps://www.cpc.com.tw/historyprice.aspx?n=2890',
+    '中油油價':'中油油價連結:\nhttps://www.cpc.com.tw/historyprice.aspx?n=2890',
     '回家山線':'!! 注意 !! 中油生產路站有"自助加油" \n總里程:45.9K \nhttps://goo.gl/maps/WskMQwJXf6uP6rZf9',
     '回家海線':'!! 注意 !! 路途沒有"自助加油" \n總里程:47.5K \nhttps://goo.gl/maps/E8Zyvf42wsh756HH9',
     '回學校':'!! 注意 !! 中油橋頭站有"自助加油" \n總里程:45.8K \nhttps://goo.gl/maps/seGtHHMQ3wCHkFrr6',
