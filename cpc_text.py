5ad5266afe68cf9136ec03e62f112160db921200
sheng411 <isu11003058A@go.isu.edu.tw>
2024-01-06 03:08:15 +0800

    update some programs and bugs and integrate query

--- cpc_text.py
+++ cpc_text.py
@@ -11,12 +11,24 @@ import requests
 import re
 import json
 
-oil_prices=[]
-oil_new_time=[]
-difference=0
 
-def CPC_oil_prices():
+
+def CPC_oil_prices(oil_type):
+    oil_prices=[]
+    oil_new_time=[]
+    difference=0
+    oil_name=""
+
     url = "https://www.cpc.com.tw/historyprice.aspx?n=2890"
+
+    oil_type=int(oil_type)
+    
+    if oil_type==98:
+        oil_name="98 無鉛汽油"
+    elif oil_type==95:
+        oil_name="95 無鉛汽油"
+    else:
+        oil_name="92 無鉛汽油"
 
     resp = requests.get(url)
     m = re.search("var pieSeries = (.*);", resp.text)
@@ -26,7 +38,8 @@ def CPC_oil_prices():
 
     for item in reversed(j):
         for data in item['data']:
-            if data['name']=="95 無鉛汽油":
+            if data['name']==oil_name:
+                #print(oil_name)
                 oil_prices.append(data['y'])
                 oil_new_time.append(item['name'])
     #print("oil-->",oil_prices)
@@ -34,14 +47,14 @@ def CPC_oil_prices():
     difference=oil_prices[0]-oil_prices[1]
     
     if difference>0:
-        msg=f"最新一筆資料時間\n{oil_new_time[0]}\n\n95無鉛汽油: {oil_prices[0]}\n\n*油價上漲*, 漲幅: {round(abs(difference), 1)}"
+        msg=f"最新一筆資料時間\n{oil_new_time[0]}\n\n{oil_name}: {oil_prices[0]}\n\n*油價上漲*, 漲幅: +{round(abs(difference), 1)}"
         return msg
     elif difference<0:
-        msg=f"最新一筆資料時間\n{oil_new_time[0]}\n\n95無鉛汽油: {oil_prices[0]}\n\n*油價下跌*, 跌幅: {round(abs(difference), 1)}"
+        msg=f"最新一筆資料時間\n{oil_new_time[0]}\n\n{oil_name}: {oil_prices[0]}\n\n*油價下跌*, 跌幅: -{round(abs(difference), 1)}"
         return msg
         #return "\noil down",round(abs(difference), 1),"\nLatest Information Time->",oil_new_time[0]
     else:
-        return f"最新一筆資料時間\n{oil_new_time[0]}\n\n95無鉛汽油: {oil_prices[0]}\n\n*油價不變動*{round(abs(difference), 1)}"
+        return f"最新一筆資料時間\n{oil_new_time[0]}\n\n{oil_name}: {oil_prices[0]}\n\n*油價不變動* {round(abs(difference), 1)}"
     return msg
 
 #print(CPC_oil_prices())
