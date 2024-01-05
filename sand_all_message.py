21de5bb7b1288f631b4e4272cd42df73740fb644
sheng411 <isu11003058A@go.isu.edu.tw>
2024-01-06 03:24:45 +0800

    update message Input area

--- sand_all_message.py
+++ sand_all_message.py
@@ -9,9 +9,12 @@ channel_access_token = iot_token
 user_ids = read_user_ids()
 
 #"This's a test message from a group.\n\nIf you receive this message, please reply to me."
-text="*update*\n工程師已將雷達回波圖已完成搶修,還順便休息一下."
+type_in=input("type(New/Update)-->")
+text_input=input("msg->")
+text=f"*{type_in}*\n{text_input}"
 
-# 訊息內容
+
+#message
 message = {
     "type": "text",
     "text": text
@@ -20,22 +23,22 @@ message = {
 # LINE Messaging API 的發送訊息的 API 網址
 api_url = f"https://api.line.me/v2/bot/message/multicast"
 
-# 設定 HTTP 標頭
+# setting HTTP header
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {channel_access_token}"
 }
 
-# 發送 POST 要求
+# send POST request
 response = requests.post(api_url, headers=headers, json={
     "to": user_ids,         #Send to users who are on the list
     "messages": [message]
 })
 
-# 檢查是否成功
+# check
 if response.status_code == 200:
-    print("訊息已成功發送\n")
+    print("Message successfully sent\n")
     print(text,"\n")
 else:
-    print(f"發送訊息失敗，錯誤碼：{response.status_code}\n\n")
+    print(f"Failed to send message,Error code:{response.status_code}\n\n")
     print(response.text)
